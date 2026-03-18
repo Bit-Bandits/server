e592b34a43ac9dcb25c7d11eee065c82148a509e
Elhelw, Mohamed <114217025+melhelow@users.noreply.github.com>
2026-03-18 01:04:20 -0500

    Add CORS middleware for separate frontend deployment

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
-const path = require("path");
+const cors = require("cors");
 const { authMiddleware } = require("./utils/auth");
 const db = require("./config/connection");
 const { typeDefs, resolvers } = require("./schemas");
@@ -14,17 +14,13 @@ const server = new ApolloServer({
   context: authMiddleware,
 });
 
+app.use(cors({
+  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  credentials: true,
+}));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-}
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
 
 
 const startApolloServer = async () => {
